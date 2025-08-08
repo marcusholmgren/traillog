f1340bcbe60ec0ab811204cca9cb0f61212de065
Marcus Holmgren <marcus.holmgren1@gmail.com>
2025-08-08 17:46:02 +0200

    chore: upgrade dependencies

--- app/routes/create_route.test.tsx
+++ app/routes/create_route.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router";
 import CreateRoute from "./create_route";
 import * as db from "~/services/db";
 import type { Waypoint } from "~/services/db"; // Ensure Waypoint type is imported
